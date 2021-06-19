23d6b786263ea202105ed6bad18eaf94dbb8858f
Arun Chapagain <arunsansar123@gmail.com>
2021-06-19 15:07:26 +0545

    first post

--- App.js
+++ App.js
@@ -1,13 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+  SafeAreaView,
+  Image,
+  Button,
+  Alert,
+  Platform,
+  StatusBar,
+} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello world</Text>
+      {/* <TouchableHighlight onPress={() => console.log('Image tapped')}>
+        <Image
+          // blurRadius={10}
+          // fadeDuration={10000}
+          source={{
+            width: 200,
+            height: 200,
+            uri: 'https://picsum.photos/200 ',
+          }}
+        ></Image>
+      </TouchableHighlight> */}
+      <Button
+        color="#841584"
+        title="Click me"
+        onPress={() =>
+          Alert.alert('Delete box', 'Do you want to delete?', [
+            { text: 'Yes', onPress: () => console.log('You pressed Yes') },
+            { text: 'No', onPress: () => console.log('You pressed NO') },
+          ])
+        }
+
+        // This works on IOS only
+        // onPress={() =>
+        //   Alert.prompt('Category', 'Please add a category', (text) => {
+        //     console.log(text);
+        //   })
+        // }
+      ></Button>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +52,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
