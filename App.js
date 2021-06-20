c94566f53959e644c84e948e4722620bf7886a2a
Arun Chapagain <arunsansar123@gmail.com>
2021-06-20 13:45:26 +0545

    Layo_Mosh

--- App.js
+++ App.js
@@ -25,11 +25,55 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <View
         style={{
-          backgroundColor: 'blue',
-          width: '50%',
-          height: landscape ? 100 : '30%',
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'center',
+          // Align items determine alignment of items within each line
+          alignItems: 'center',
+          flexWrap: 'wrap',
+          // align content aligns all the content, it works with wrapping only
+          // It does not have effect without wrapping
+          alignContent: 'center',
         }}
-      ></View>
+      >
+        <View
+          style={{
+            backgroundColor: 'dodgerblue',
+            height: 100,
+            width: 100,
+            // alignSelf: 'flex-end',
+          }}
+        ></View>
+        <View
+          style={{
+            backgroundColor: 'gold',
+            height: 100,
+            width: 100,
+            top: 100,
+          }}
+        />
+        <View
+          style={{
+            backgroundColor: 'tomato',
+            height: 100,
+            width: 100,
+          }}
+        />
+        {/* <View
+          style={{
+            backgroundColor: 'brown',
+            height: 100,
+            width: 100,
+          }}
+        />
+        <View
+          style={{
+            backgroundColor: 'greenyellow',
+            height: 100,
+            width: 100,
+          }}
+        /> */}
+      </View>
       {/* <Text>Hello world</Text> */}
       {/* <TouchableHighlight onPress={() => console.log('Image tapped')}>
         <Image
